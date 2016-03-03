51f236a35b3a597dac1d7817d25a62eae5655407
olanmj <olanm@stockton.edu>
2016-03-03 13:49:34 -0500

    FIxed labels and made TItle required

--- Music/Models/Album.cs
+++ Music/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,15 @@ namespace Music.Models
 {
     public class Album
     {
+
         public int AlbumID { get; set; }
+        [Required(ErrorMessage ="Album title is required")]
         public string Title { get; set; }
+        [Display(Name ="Genre")]
         public int GenreID { get; set; }
         public Genre Genre { get; set; }
         public decimal Price { get; set; }
+        [Display(Name = "Artist")]
         public int ArtistID { get; set; }
         public Artist Artist { get; set; }
 
