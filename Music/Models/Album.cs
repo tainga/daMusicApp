bf65314adde13792ba7eb2ec9be8d7bfde43b436
olanmj <olanm@stockton.edu>
2016-03-03 13:08:08 -0500

    Initial commit with models

---
+++ Music/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public int GenreID { get; set; }
+        public Genre Genre { get; set; }
+        public decimal Price { get; set; }
+        public int ArtistID { get; set; }
+        public Artist Artist { get; set; }
+
+    }
+}
